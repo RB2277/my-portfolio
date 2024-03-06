f31530b3f2305474b44967dac7124475f5c0421a
RB <jsriley24505@gmail.com>
2024-03-05 20:05:03 -0600

    added bootstrap to project. began to setup component contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <></>
+    <>
+      <Header />
+
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 
 function App() {
 
 
   return (
-    <></>
+    <div>
+<header>
+   <div>
+
+   </div>
+</header>
+    </div>
   )
 }
 
