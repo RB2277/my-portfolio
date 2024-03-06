a4530a01ae9be81e2dcd37f6e614879fae17f3f4
RB <jsriley24505@gmail.com>
2024-03-06 09:42:48 -0600

    added Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+function Navigation() {
+
+
+  return (
+    <nav>
+<a>About Me</a>
+<a>Portfolio</a>
+ <a>Resume</a>
+ <a>Contact</a>
+    </nav>
+  )
+}
+
+export default Navigation
+
+
+
+
+
+
+
+
+
