2a4b3ab2d4b34a77c238d6d391bba1dc23791393
RB <jsriley24505@gmail.com>
2024-03-05 20:26:00 -0600

    added content to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,37 @@
-import { useState, useEffect } from 'react'
-import "/node_modules/bootstrap/dist/css/bootstrap.min.css"
-
+import { useState, useEffect } from "react";
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
-
   return (
-    <div>
-<header>
-   <div>
+    <div className="container">
+            <div className="row">
+      <header>
+            <div className="col-3">
+              <h1 id="name">Riley Burns</h1>
+            </div>
+            <div className="col-9">
+              <nav>
+               <ul>
+                  <li>
+                   <a>About Me</a>
+                 </li>
+                  <li>
+                   <a>Portfolio</a>
+                 </li>
+                  <li>
+                    <a>Resume</a>
+                 </li>
+                  <li>
+                    <a>Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
 
-   </div>
-</header>
+      </header>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
