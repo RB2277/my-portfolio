8c1cd61ef5995c8558e0fed2b286c2e3ca668896
RB <jsriley24505@gmail.com>
2024-03-09 00:44:09 -0600

    changed file paths for images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ function Footer() {
         <div className="col-6" id="logoDiv">
 
           <a href="https://github.com/RB2277">
-            <img src="../../public/github.png" alt="small GitHub logo" className="footerLogo"/>
+            <img src="/github.png" alt="small GitHub logo" className="footerLogo"/>
           </a>
           <a href="https://www.linkedin.com/in/riley-burns-4357b72b1/">
-            <img src="../../public/linkedin.png" alt="small Linkedin logo" className="footerLogo" />
+            <img src="/linkedin.png" alt="small Linkedin logo" className="footerLogo" />
           </a>
           <a href="https://github.com/RB2277">
-            <img src="../../public/leetcode.png" alt="small LeetCode logo" className="footerLogo"/>
+            <img src="/leetcode.png" alt="small LeetCode logo" className="footerLogo"/>
           </a>
         </div>
       </div>
