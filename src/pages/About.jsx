c84e23f8964be8a50334b54b4a2c1368e5d7652f
RB <jsriley24505@gmail.com>
2024-03-17 16:31:11 -0500

    redesigned the entire about me component. made it responsive

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,19 @@
-function About() {
+export default function About() {
   return (
-    <div className="row" id="aboutDiv">
-      <div className="col-12">
-        <section className="aboutMe">
-          <img id="personalPic" src="/portrait.jpg" alt="A picture of me"></img>
-
-
-          <h2>Hello!</h2>
-          <p>
-            My name is Riley Burns. A married man as of September, 2023, and am a proud cat dad to two adorable felines. I've used computers throughout my whole life, and have always been interested in taking the next step.
-            That's when I decided to give it my all and sign up for the University of Minnesota's 12 week Fullstack bootcamp. Throughout this program, I've been mastering the fundamentals of web development, cultimating
-            into the creation of my own personal portfolio using React. 
-            </p>
-            <p id="personalAbout">
-            Some of the things I enjoy in my free time outside of strengthening my coding knowledge is studying history, spending time with my family, and gaming. Some of my favorite games are Sid Meier's Civilization
-            6 and Old School Runescape. Reading, especially historical literature, is another one of my hobbies. Recently I've began to read The Canterbury Tales by Geoffrey Chaucer</p>
-
-
-        </section>
+    <div className="container d-flex">
+    <div className="row align-items-center">
+      <div className="col-md-6">
+        <h2 className="d-flex justify-content-center border-bottom mb-4">Hello!</h2>
+        <p>
+          My name is Riley Burns. A married man as of September, 2023, and am a proud cat dad to two adorable felines. I've used computers throughout my whole life, and have always been interested in taking the next step.
+          That's when I decided to give it my all and sign up for the University of Minnesota's 12 week Fullstack bootcamp. Throughout this program, I've been mastering the fundamentals of web development, culminating
+          into the creation of my own personal portfolio using React.
+        </p>
+      </div>
+      <div className="col-md-6  col-sm-12 d-flex justify-content-center">
+        <img id="personalPic" src="/portrait.jpg" alt="A picture of me" className="img-fluid rounded-5 border border-dark"></img>
       </div>
     </div>
+  </div>
   );
 }
-
-export default About;
